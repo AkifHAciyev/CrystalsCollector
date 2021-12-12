2f2739e5247095de7d05d9382206f1e9c057b8e1
AkifHaciyev <akif6563@mail.ru>
2021-12-12 16:41:54 +0400

    the end

---
+++ assets/javascript/game.js
@@ -0,0 +1,119 @@
+$('document').ready(function () {
+	var wins = 0;
+	var losses = 0;
+	var totalScore = 0;
+
+	// Generate and show random number
+	var randomNumber = Math.floor(Math.random() * 101) + 19;
+	$('#random-number').text(randomNumber);
+
+	// Assign random numbers to  crystals
+	var crystal = Math.floor(Math.random() * 11) + 1;
+	$('#crystal').val(crystal);
+	console.log(crystal);
+	var emerald = Math.floor(Math.random() * 11) + 1;
+	$('#emerald').val(emerald);
+	console.log(emerald);
+	var sapphire = Math.floor(Math.random() * 11) + 1;
+	$('#sapphire').val(sapphire);
+	console.log(sapphire);
+	var zircon = Math.floor(Math.random() * 11) + 1;
+	$('#zircon').val(zircon);
+	console.log(zircon);
+
+	// Show wins and losses
+	$('#result').text("Let's go!");
+	$('#wins').text('Wins: ' + wins);
+	$('#losses').text('Losses: ' + losses);
+
+	function gameStart() {
+		randomNumber = ~~(Math.random() * 101) + 19;
+		$('#random-number').text(randomNumber);
+
+		crystal = ~~(Math.random() * 11) + 1;
+		$('#crystal').val(crystal);
+		console.log(crystal);
+		emerald = ~~(Math.random() * 11) + 1;
+		$('#emerald').val(emerald);
+		console.log(emerald);
+		sapphire = ~~(Math.random() * 11) + 1;
+		$('#sapphire').val(sapphire);
+		console.log(sapphire);
+		zircon = ~~(Math.random() * 11) + 1;
+		$('#zircon').val(zircon);
+		console.log(zircon);
+
+		totalScore = 0;
+
+		$('#scorecounter').text(totalScore);
+	}
+
+	// Click events
+
+	$('#crystal').on('click', function () {
+		totalScore += parseInt(crystal);
+		$('#scorecounter').text(totalScore);
+		if (totalScore === randomNumber) {
+			$('#result').text('You won!');
+			wins++;
+			$('#wins').text('Wins: ' + wins);
+			gameStart();
+		} else if (totalScore > randomNumber) {
+			$('#result').text('You lost');
+			losses++;
+			$('#losses').text('Losses: ' + losses);
+			gameStart();
+		}
+	});
+
+	$('#emerald').on('click', function () {
+		totalScore += parseInt(emerald);
+		$('#scorecounter').text(totalScore);
+
+		if (totalScore === randomNumber) {
+			$('#result').text('You won!');
+			wins++;
+			$('#wins').text('Wins: ' + wins);
+			gameStart();
+		} else if (totalScore > randomNumber) {
+			$('#result').text('You lost');
+			losses++;
+			$('#losses').text('Losses: ' + losses);
+			gameStart();
+		}
+	});
+
+	$('#sapphire').on('click', function () {
+		totalScore += parseInt(sapphire);
+		$('#scorecounter').text(totalScore);
+
+		if (totalScore === randomNumber) {
+			$('#result').text('You won!');
+			wins++;
+			$('#wins').text('Wins: ' + wins);
+			gameStart();
+		} else if (totalScore > randomNumber) {
+			$('#result').text('You lost');
+			losses++;
+			$('#losses').text('Losses: ' + losses);
+			gameStart();
+		}
+	});
+
+	$('#zircon').on('click', function () {
+		totalScore += parseInt(zircon);
+		$('#scorecounter').text(totalScore);
+
+		if (totalScore === randomNumber) {
+			$('#result').text('You won!');
+			wins++;
+			$('#wins').text('Wins: ' + wins);
+			gameStart();
+		} else if (totalScore > randomNumber) {
+			$('#result').text('You lost');
+			losses++;
+			$('#losses').text('Losses: ' + losses);
+			gameStart();
+		}
+	});
+});
